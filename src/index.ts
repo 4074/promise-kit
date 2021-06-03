d26cb2d390228f49f05b4f46f9f87a8528d17125
4074 <137657976@qq.com>
2021-06-04 01:30:05 +0800

    Add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
+export class PromisePool {
+  public resolves: CallableFunction[] = []
+  public size = 0
+  public running = 0
 
-type CallablePromiseFunction = (...args: any[]) => Promise<any>
+  public constructor(size: number) {
+    this.size = size
+  }
 
-export default class PromisePool {
-  resolves: CallableFunction[] = []
-  running = 0
-
-  constructor(public size: number) { }
-
-  execute = (fn: CallablePromiseFunction) => {
-    if (this.running === this.size) {
+  public execute = async <T>(fn: () => Promise<T>): Promise<T> => {
+    if (this.size && this.running === this.size) {
       return new Promise((resolve) => {
         this.resolves.push(resolve)
       }).then(fn)
     }
 
     return fn().then((data) => {
-      if (this.resolves.length) {
-        this.resolves.shift()()
-      } else {
-        this.running -= 1
-      }
+      this.resolves.length ? this.resolves.shift()() : this.running -= 1
       return data
     })
+  }
+}
+
+export default <P extends any[], T>(fn: (...args: P) => Promise<T>, size: number) => {
+  const pool = new PromisePool(size)
+  return (...args: P) => {
+    return pool.execute(fn.bind(null, ...args))
   }
 }
